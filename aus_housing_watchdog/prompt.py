ddbe5d8cc1dcca55375e69e3192f4eaed65d8dd3
Kanishka <kanishkawickramasinghe50@gmail.com>
2025-06-19 16:41:49 +1000

    removed unecessary files

--- aus_housing_watchdog/prompt.py
+++
@@ -1,6 +0,0 @@
-
-root_agent_instruction = """
-You are an assistant designed to help users find a suitable suburb to purchase a property in NSW, Australia.
-Your main task is to collect the user's budget and any preferred suburbs.
-After gathering this information, call your tools to find a suitable suburb.
-"""
